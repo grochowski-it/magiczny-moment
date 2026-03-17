78860f87ad35417eff2e2461ff92f56bad912db5
mrGrochowski <mateusz.grochowski1992@gmail.com>
2026-03-17 19:48:14 +0100

    Revert "fix(layout): Fix content-grid architecture and Embla hydration issues for OfferCarousel. Replaced serverQueryCollection with correct queryCollection in sitemap generation. Removed local testing artifacts."
    This reverts commit f6651d4fa8421d3c1053ed6dc2367de9b14efc0d.

--- components/content/OfferCarousel.vue
+++ components/content/OfferCarousel.vue
@@ -1,34 +1,32 @@
 <template>
   <div class="flex flex-col justify-end w-full py-4 md:justify-center h-section">
     <h1 class="mb-8 text-4xl font-bold leading-snug md:text-6xl">Oferta</h1>
-    <div class="relative w-full">
-      <ClientOnly>
-        <div class="overflow-hidden w-full" ref="emblaRef">
+    <div class="relative w-full embla">
+        <div class="overflow-hidden w-full embla__viewport" ref="emblaRef">
           <div
             v-if="offers?.length"
-            class="flex h-[calc(var(--section-height)*0.7)] gap-4 md:gap-16">
+            class="flex h-[calc(var(--section-height)*0.7)] gap-4 md:gap-16 embla__container">
             <template v-for="(offer, index) in offers" :key="offer.path">
-              <div class="flex-[0_0_70%] md:flex-[0_0_calc(50%-2rem)] min-w-0">
+              <div class="flex-[0_0_70%] md:flex-[0_0_calc(50%-2rem)] min-w-0 embla__slide">
                 <Card :image="offer.image || offer.meta?.image" :title="offer.title || offer.meta?.title" :content="offer.content || offer.meta?.content" :link="offer.path"
                   :class="{ 'mr-4 md:mr-16': index == offers.length - 1 }" />
               </div>
             </template>
           </div>
         </div>
-        <button class="hidden md:block absolute transform -translate-y-1/2 top-1/2 left-[-5ch]" @click="scrollPrev">
+        <button class="hidden md:block absolute transform -translate-y-1/2 top-1/2 left-[-5ch] embla__prev" @click="scrollPrev">
           <Icon name="icon-park-outline:arrow-left" />
         </button>
-        <button class="hidden md:block absolute transform -translate-y-1/2 top-1/2 right-[-5ch]" @click="scrollNext">
+        <button class="hidden md:block absolute transform -translate-y-1/2 top-1/2 right-[-5ch] embla__next" @click="scrollNext">
           <Icon name="icon-park-outline:arrow-right" />
         </button>
-      </ClientOnly>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import emblaCarouselVue from 'embla-carousel-vue'
-const [emblaRef, emblaApi] = emblaCarouselVue({ loop: true })
+const [emblaRef, emblaApi] = emblaCarouselVue({ loop: true , align: 'center', watchSlides:true})
 
 const { data: offers } = await useAsyncData('oferta', () => {
   return queryCollection('oferta').all()
@@ -42,3 +40,8 @@ const scrollPrev = () => {
   emblaApi.value?.scrollPrev()
 }
 </script>
+<style lang="css">
+.embla__container {
+touch-action: pan-y pinch-zoom;
+}
+</style>
--- content/index.md
+++ content/index.md
@@ -2,29 +2,23 @@
 title: Strona główna
 ---
 
+:navigation
+
 ::witamy
 ---
-title: "Magiczny Moment"
-h1: "Zabawa na Najwyższym Poziomie!"
-sub: "Odkryj atrakcje, które zaskoczą Twoich gości."
-image: "/457065765_122154398708052132_8698480936273943806_n.jpg"
-class: "full-width"
-id: "witamy"
-link: "oferta"
+class: full-width
+h1: Zabawa na Najwyższym Poziomie!
+id: witamy
+image: /457065765_122154398708052132_8698480936273943806_n.jpg
+link: oferta
+sub: Odkryj atrakcje, które zaskoczą Twoich gości.
+title: Magiczny Moment
 ---
 ::
 
-::offer-carousel
----
-id: "oferta"
----
-::
+:offer-carousel{#oferta}
 
-::contact
----
-id: "kontakt"
-class: "full-width"
----
+::contact{#kontakt .full-width}
 Zapraszamy do kontaktu od poniedziałku do piątku w godzinach 08:00 - 20:00
 
 Telefon: [+48 602 434 673](tel:+48602434673)
